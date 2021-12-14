b4fbb4077b91d42e24d85bdb00384f4851f4536b
reyhan8trkkl <94196332+reyhan8trkkl@users.noreply.github.com>
2021-12-14 11:51:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-let myAge = prompt("yasinizi girin");
-let earlyYears = 2;
-earlyYears = earlyYears *=10.5 ;
 
-let laterYears = myAge - 2;
-laterYears = laterYears * 4;
+const Kelvin=0;
+const celsius= Kelvin-273;
+let fahrenheit=celsius*(9/5)+32;
 
-console.log(laterYears);
-console.log(earlyYears);
+fahrenheit = Math.round(fahrenheit);
 
-let myAgeInDogYears = earlyYears + laterYears;
-
-let myName = "Reyhan";
-console.log(myName.toLowerCase());
-
-console.log(`My namme is ${myName} .I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
-
-let kopekyasi= 129;
-let ilk2yil= 10.5*2;
-let yas = kopekyasi-ilk2yil;
-let sonyas= yas/4+2;
-console.log(sonyas);
+console.log(`
+ Sicaklik ${fahrenheit}
+`);
